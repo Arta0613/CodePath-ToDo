225edb04fe20b1f0f30b1e13bf5568e901a40d78
Mohammed Amin <Mohammed.Amin@viacom.com>
2016-11-21 10:20:34 -0500

    Replace default toolbar with custom Toolbar

--- app/src/main/java/com/rushlimit/codepath_todo/MainActivity.java
+++ app/src/main/java/com/rushlimit/codepath_todo/MainActivity.java
@@ -2,6 +2,10 @@ package com.rushlimit.codepath_todo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.miCompose:
+                Toast.makeText(MainActivity.this, "miCompose", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.miProfile:
+                Toast.makeText(MainActivity.this, "miProfile", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
