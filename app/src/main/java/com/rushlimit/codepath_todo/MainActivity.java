eb11b1f53122a6cdce08e52d0a376b4ddae62c5c
Mohammed Amin <Mohammed.Amin@viacom.com>
2016-12-13 16:34:37 -0500

    Add ability to add new todos from app

--- app/src/main/java/com/rushlimit/codepath_todo/MainActivity.java
+++ app/src/main/java/com/rushlimit/codepath_todo/MainActivity.java
@@ -5,16 +5,22 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<String> items;
-    ArrayAdapter<String> itemsAdapter;
-    ListView lvItems;
+    private Button todoAddButton;
+    private EditText todoToAdd;
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +30,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        todoAddButton = (Button) findViewById(R.id.todoAddButton);
+        todoToAdd = (EditText) findViewById(R.id.todoToAdd);
+
         lvItems = (ListView) findViewById(R.id.todoListView);
         items = new ArrayList<>();
         itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
-        itemsAdapter.add("First Item");
-        itemsAdapter.add("Second Item");
     }
 
     @Override
@@ -51,5 +58,23 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void addItem(View view) {
+        if (todoToAdd == null) {
+            Toast.makeText(MainActivity.this,
+                    R.string.something_went_wrong, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String textToAdd = todoToAdd.getText().toString();
+        if (textToAdd.isEmpty()) {
+            Toast.makeText(MainActivity.this,
+                    R.string.all_tasks_complete, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        itemsAdapter.add(textToAdd);
+        todoToAdd.setText("");
     }
 }
